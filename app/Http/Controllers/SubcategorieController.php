87c0d8ddc7fe7a6db5624d10614d2611869149df
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2025-03-11 14:28:57 +0000

    ajouter migration categorie et subcategrie et tag

---
+++ app/Http/Controllers/SubcategorieController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\subcategorie;
+use App\Http\Requests\StoresubcategorieRequest;
+use App\Http\Requests\UpdatesubcategorieRequest;
+
+class SubcategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoresubcategorieRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(subcategorie $subcategorie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(subcategorie $subcategorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatesubcategorieRequest $request, subcategorie $subcategorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(subcategorie $subcategorie)
+    {
+        //
+    }
+}

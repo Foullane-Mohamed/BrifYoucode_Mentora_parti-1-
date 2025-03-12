08534e20c5f5ce0aeb9aad7aca01f31d4cf1fc0f
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2025-03-12 11:09:17 +0000

    ajouter table pivo pour les inscription

---
+++ database/migrations/2025_03_12_110453_create-courses-table.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['draft', 'published'])->default('draft');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+
+        Schema::create('course_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('progress')->default(0);
+            $table->timestamps();
+
+            $table->unique(['course_id', 'user_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('course_user');
+        Schema::dropIfExists('courses');
+    }
+};
